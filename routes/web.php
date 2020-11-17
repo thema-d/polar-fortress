631a223b7e29b616ddad5a9bb61fb27051452db5
Bekele Debisa <bdebisa@gmail.com>
2020-11-18 00:35:42 +0300

    single product UI page done

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('product.list');
+});
+
+Route::get('single-product', function() {
+    return view('product.single');
 });
