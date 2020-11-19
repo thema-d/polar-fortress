eef09a10a9dc5c20c525131a5684748a8df37504
Bekele Debisa <bdebisa@gmail.com>
2020-11-19 10:40:58 +0300

    a minor change

--- resources/views/product/cart.blade.php
+++ resources/views/product/cart.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.app') @section('bread_crumb')
 <ul class="breadcrumb-list">
-  <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+  <li class="breadcrumb-item"><a href="#">Home</a></li>
   <li class="breadcrumb-item active">My Cart</li>
 </ul>
 @endsection @section('content')
