9786d1ec2ace63fb43d205b5708a064b1fdbae18
Bekele Debisa <bdebisa@gmail.com>
2020-11-19 10:37:17 +0300

    created a product factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        /**
+         * List of product images at our disposal
+         * 
+         * @var array
+         */
+        $images = [
+            'small' => [
+                '/assets/img/product/small/1.jpg',
+                '/assets/img/product/small/2.jpg',
+                '/assets/img/product/small/3.jpg',
+            ],
+            'medium' => collect(range(1, 20))->map(fn ($n) => "/assets/img/product/medium/$n.jpg")->toArray(),
+            'large' => [
+                'assets/img/product/large/lg-product-1.jpg',
+                'assets/img/product/large/lg-product-2.jpg',
+                'assets/img/product/large/lg-product-3.jpg',
+                'assets/img/product/large/lg-product-4.jpg',
+                'assets/img/product/large/lg-product-5.png',
+                'assets/img/product/large/lg-product-6.png',
+            ],
+        ];
+
+        /**
+         * A helper function to return random images
+         * 
+         * @return array
+         */
+        $get_random_images = fn () => [
+            'small' => $this->faker->randomElements($images['small'], 1),
+            'medium' => $this->faker->randomElements($images['medium'], 1),
+            'large' => $this->faker->randomElements($images['large'], 4),
+        ];
+
+        return [
+            'name' => $this->faker->name,
+            'size' => $this->faker->randomElement(['SMALL', 'MEDIUM', 'LARGE']),
+            'color' => $this->faker->safeColorName,
+            'price' => $this->faker->numberBetween(100 * 100, 1000 * 100), // in cents
+            'images' => $get_random_images(),
+            'gender' => $this->faker->randomElement(['MALE', 'FEMALE']),
+            'available_amount' => $this->faker->numberBetween(10, 100),
+            'rating' => $this->faker->numberBetween(10, 50) / 10, // 1.0 - 5.0
+            'discount' => $this->faker->boolean() ? $this->faker->numberBetween(1, 5) * 10 : null,
+        ];
+    }
+}
---
+++ database/migrations/2020_11_19_055426_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('size', ['SMALL', 'MEDIUM', 'LARGE']);
+            $table->string('color');
+            $table->integer('price');
+            $table->text('images');
+            $table->enum('gender', ['MALE', 'FEMALE']);
+            $table->integer('available_amount')->default(0);
+            $table->integer('rating')->nullable();
+            $table->float('discount')->nullable()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
